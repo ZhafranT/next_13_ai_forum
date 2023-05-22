7d7810d1560ed07628ea9fec01097ab6767b99c9
ZhafranT <zhaftosa@gmail.com>
2023-05-22 14:15:02 +0700

    create page done

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,17 +1,56 @@
 "use client";
 
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 import Form from "@components/Form";
 
 const CreatePrompt = () => {
-   return <div>CreatePrompt</div>;
-};
+   const router = useRouter();
+   const { data: session } = useSession();
 
-{
-   /* 1:51:12 */
-}
+   const [submitting, setSubmitting] = useState(false);
+   const [post, setPost] = useState({
+      post: "",
+      tag: "",
+   });
+
+   const createPrompt = async (e) => {
+      e.preventDefault();
+
+      // for loader / loading
+      setSubmitting(true);
+
+      try {
+         const res = await fetch("api/prompt/new", {
+            method: "POST",
+            body: JSON.stringify({
+               prompt: post.prompt,
+               userId: session?.user.id,
+               tag: post.tag,
+            }),
+         });
+
+         if (res.ok) {
+            router.push("/");
+         }
+      } catch (error) {
+         console.log(error);
+      } finally {
+         setSubmitting(false);
+      }
+   };
+
+   return (
+      <Form
+         type="Create "
+         post={post}
+         setPost={setPost}
+         submitting={submitting}
+         handleSubmit={createPrompt}
+      />
+   );
+};
 
 export default CreatePrompt;
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -5,7 +5,6 @@ import Link from "next/link";
 
 import {
    getProviders,
-   sigIn,
    sigOut,
    signIn,
    signOut,
